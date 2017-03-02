8ffe4bd543dfa7553cac35856a9e1b349b5ef226
unknown <brucebigirwenkya@gmail.com>
2017-03-02 11:28:30 +0300

    PWA Building

---
+++ sw-precache-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  staticFileGlobs: [
+    'dist/**.html',
+    'dist/**.js',
+    'dist/**.css',
+    'dist/assets/*'
+  ],
+  root: 'dist',
+  stripPrefix: 'dist/',
+  navigateFallback: '/index.html',
+  runtimeCaching: [{
+    urlPattern: /eradapi\.herokuapp\.com/,
+    handler: 'networkFirst'
+  }]
+};
